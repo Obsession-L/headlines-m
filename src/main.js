aa43bbc7c2e84dacfda6f47f76e8eebacc202342
My_slience <1769252516@qq.com>
2022-08-14 10:33:05 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less' // 引入全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css' // 引入vant样式
+import 'amfe-flexible' // 引入移动端rem适配工具
 
 Vue.config.productionTip = false
+Vue.use(Vant) // 挂载vant
 
 new Vue({
   router,
